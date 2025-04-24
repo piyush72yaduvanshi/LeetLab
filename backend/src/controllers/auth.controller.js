a49c4e5aad0e906b9f484eb627ae8a7cf0471235
Piyush Yadav <151051765+piyush72yaduvanshi@users.noreply.github.com>
2025-04-25 00:39:45 +0530

    authentcation is added

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,134 @@
+import bcrypt from "bcryptjs";
+import { db } from "../libs/db.js";
+import { UserRole } from "../generated/prisma/index.js";
+import jwt from "jsonwebtoken";
+import cookieParser from "cookie-parser";
+
+export const register = async (req, res) => {
+  const { name, email, password } = req.body;
+  try {
+    const existingUser = await db.user.findUnique({
+      where: {
+        email,
+      },
+    });
+    if (existingUser) {
+      return res.status(400).json({
+        message: "User already exists",
+      });
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = await db.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+        role: UserRole.USER,
+      },
+    });
+
+    const token = jwt.sign({ id: newUser.id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+    res.cookie("jwt", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV !== "development",
+      sameSite: "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+
+    res.status(201).json({
+      success: true,
+      message: "User created successfully",
+      user: {
+        id: newUser.id,
+        name: newUser.name,
+        email: newUser.email,
+        role: newUser.role,
+        image: newUser.image,
+      },
+    });
+  } catch (error) {
+    console.log("Error creating user:", error);
+    res.status(500).json({
+      error: "Error creating user",
+    });
+  }
+};
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await db.user.findUnique({
+      where: {
+        email,
+      },
+    });
+    if (!user) {
+      return res.status(401).json({ error: "User not found" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(401).json({ error: "Invalid credentials" });
+    }
+
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+    res.cookie("jwt", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV !== "development",
+      sameSite: "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+
+    res.status(200).json({
+      success: true,
+      message: "Login successful",
+      user: {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+        image: user.image,
+      },
+    });
+  } catch (error) {
+    console.log("Error logging in:", error);
+    res.status(500).json({ error: "Error logging in" });
+  }
+};
+
+export const logout = async (req, res) => {
+  try {
+    res.clearCookie("jwt", {
+      httpOnly: true,
+      secure: process.env.NODE_ENV !== "development",
+      sameSite: "strict",
+    });
+
+    res.status(200).json({
+      success: true,
+      message: "Logout successful",
+    });
+  } catch (error) {
+    console.log("Error logging out:", error);
+    res.status(500).json({ error: "Error logging out" });
+  }
+};
+
+export const check = async (req, res) => {
+    try {
+        res.status(200).json({
+            success: true,
+            message: "User is authenticated",
+            user: req.user
+        })
+    } catch (error) {
+        console.log("Error checking user:", error);
+        res.status(500).json({ error: "Error checking user" });
+    }
+};
---
+++ backend/src/index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+import authRoutes from "./routes/auth.routes.js";
+
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
+
+const port = process.env.PORT || 8080;
+
+app.get("/", (req, res) => {
+    res.send("Hello 😍");
+});
+
+app.use("/api/auth", authRoutes);
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
