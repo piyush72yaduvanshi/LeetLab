eee0440ce9a7e8bc7478d67ede2eeba16ed0b8f3
Piyush Yadav <151051765+piyush72yaduvanshi@users.noreply.github.com>
2025-07-17 23:00:35 +0530

    Initialize frontend code and added signup page

---
+++ frontend/src/App.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import LoginPage from "./page/LoginPage";
+import SignUpPage from "./page/SignUpPage";
+import HomePage from "./page/HomePage";
+
+const App = () => {
+  let authUser = null;
+  return (
+    <div className="flex flex-col items-center justify-start">
+      <Routes>
+        <Route
+          path="/"
+          element={authUser ? <HomePage /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/login"
+          element={!authUser ? <LoginPage /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/signup"
+          element={!authUser ? <SignUpPage /> : <Navigate to="/" />}
+        />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
---
+++ frontend/src/store/useAuthStore.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function useAuthStore() {
+  return (
+    <div>useAuthStore</div>
+  )
+}
+
+export default useAuthStore
